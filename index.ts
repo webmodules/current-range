5f6bed74ae54fad40d9cfd1c4b9290e1708bf3c1
Nathan Rajlich <nathan@tootallnate.net>
2014-08-21 16:01:38 -0400

    initial commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+
+/**
+ * JavaScript dependencies.
+ */
+
+var getWindow = require('get-window');
+
+/**
+ * Returns a `Range` instance from the given Window or Document instance.
+ * Defaults to index `0`, but you may specify another index if desired.
+ *
+ * @param {Window|Document} doc - Window or Document instance to get the
+ *   current selection Range from.
+ * @return {Range} returns a Range instance, or `null` if there's no
+ *   current selection.
+ * @public
+ */
+
+function currentRange (doc: any, index?: number): Range {
+  var win: Window = getWindow(doc);
+  var sel: Selection = win.getSelection();
+  index |= 0;
+  if (sel.rangeCount <= index) return null;
+  return sel.getRangeAt(index);
+}
+
+export = currentRange;
